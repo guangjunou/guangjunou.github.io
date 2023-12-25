84f204a99285ce426b78f74f42b2c2cf830a473e
Guangjun Ou <JUN@Guangjuns-MacBook-Pro.local>
2023-12-25 01:59:22 -0500

    add animation

---
+++ Javascript/wave.js
@@ -0,0 +1,60 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.querySelector('.js-wave');
+    logo.addEventListener('mouseover', function() {
+        logo.classList.add('wave');
+        setTimeout( () => {
+            logo.classList.remove('wave');
+          }, 1000);
+    });
+
+    setTimeout(function() {
+        waveOnLoad();
+      }, 1000);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.querySelector('.js-wave1');
+    logo.addEventListener('mouseover', function() {
+        logo.classList.add('wave1');
+        setTimeout( () => {
+            logo.classList.remove('wave1');
+          }, 1000);
+    });
+    setTimeout(function() {
+        waveOnLoad();
+      }, 1000);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.querySelector('.js-wave2');
+    logo.addEventListener('mouseover', function() {
+        logo.classList.add('wave2');
+        setTimeout( () => {
+            logo.classList.remove('wave2');
+          }, 1000);
+    });
+
+    setTimeout(function() {
+        waveOnLoad();
+      }, 1000);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.querySelector('.js-wave3');
+    logo.addEventListener('mouseover', function() {
+        logo.classList.add('wave2');
+        setTimeout( () => {
+            logo.classList.remove('wave2');
+          }, 1000);
+    });
+
+    setTimeout(function() {
+        waveOnLoad();
+      }, 1000);
+});
