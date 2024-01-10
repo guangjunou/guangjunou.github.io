e1dd5151861e5a1e5474ac834aab8692f3d015b1
Guangjun Ou <JUN@Guangjuns-MacBook-Pro.local>
2024-01-10 10:14:05 -0500

    fix the link

--- Javascript/project.js
+++ Javascript/project.js
@@ -13,13 +13,13 @@ const projects = [
     },
     {
         name: 'SuShi Webpage',
-        url: 'https://jun-react.teacherxu.repl.co',
+        url: 'https://15600a6a-47d5-4f51-8ffb-6adb59f2c467-00-16gkwzpdpkfy7.picard.replit.dev',
         description: 'A small React project involving the replication of a sushi webpage, aiming to recreate the layout with HTML, refine its visual appeal using CSS and Bootstrap, and finally implement interactive features with JavaScript to resemble the original design.',
         used: ['HTML', 'CSS', 'JavaScript', 'Boostrap', 'React']
     },
     {
         name: 'YelpCamp',
-        url: 'https://yelpcampjun.teacherxu.repl.co',
+        url: 'https://d5e47213-8160-4c84-9d03-b10b028dd43d-00-2lwnx60ahrner.riker.replit.dev',
         description: 'A full-stack website project involving the replication of YelpCamp, allowing users to create and review campgrounds. To interact with the platform, users are required to have an account, providing a comprehensive experience for campground creation and reviews.',
         used: [ 'HTML', 'CSS', 'JavaScript', 'Boostrap', 'Node.js', 'Express', 'MongoDB', 'jQuery']
     },
